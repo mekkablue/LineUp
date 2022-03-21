19d609e3ef3bf545980390af33ccc04ba33aeb8f
Rainer Scheichelbauer <res@glyphsapp.com>
2022-03-21 19:34:45 +0000

    initial commit

---
+++ LineUp.glyphsFilter/Contents/Resources/plugin.py
@@ -0,0 +1,105 @@
+# encoding: utf-8
+
+###########################################################################################################
+#
+#
+#	Filter without dialog plug-in
+#
+#	Read the docs:
+#	https://github.com/schriftgestalt/GlyphsSDK/tree/master/Python%20Templates/Filter%20without%20Dialog
+#
+#
+###########################################################################################################
+
+from __future__ import division, print_function, unicode_literals
+import objc, math
+from GlyphsApp import *
+from GlyphsApp.plugins import *
+from AppKit import NSAffineTransform, NSAffineTransformStruct
+
+def transform(shiftX=0.0, shiftY=0.0, rotate=0.0, skew=0.0, scale=1.0):
+	"""
+	Returns an NSAffineTransform object for transforming layers.
+	Apply an NSAffineTransform t object like this:
+		Layer.transform_checkForSelection_doComponents_(t,False,True)
+	Access its transformation matrix like this:
+		tMatrix = t.transformStruct() # returns the 6-float tuple
+	Apply the matrix tuple like this:
+		Layer.applyTransform(tMatrix)
+		Component.applyTransform(tMatrix)
+		Path.applyTransform(tMatrix)
+	Chain multiple NSAffineTransform objects t1, t2 like this:
+		t1.appendTransform_(t2)
+	"""
+	myTransform = NSAffineTransform.transform()
+	if rotate:
+		myTransform.rotateByDegrees_(rotate)
+	if scale != 1.0:
+		myTransform.scaleBy_(scale)
+	if not (shiftX == 0.0 and shiftY == 0.0):
+		myTransform.translateXBy_yBy_(shiftX,shiftY)
+	if skew:
+		skewStruct = NSAffineTransformStruct()
+		skewStruct.m11 = 1.0
+		skewStruct.m22 = 1.0
+		skewStruct.m21 = math.tan(math.radians(skew))
+		skewTransform = NSAffineTransform.transform()
+		skewTransform.setTransformStruct_(skewStruct)
+		myTransform.appendTransform_(skewTransform)
+	return myTransform
+
+def calculateAngle( firstPoint, secondPoint ):
+	"""
+	Returns the angle (in degrees) of the straight line between firstPoint and secondPoint,
+	0 degrees being the second point to the right of first point.
+	firstPoint, secondPoint: must be NSPoint or GSNode
+	"""
+	xDiff = secondPoint.x - firstPoint.x
+	yDiff = secondPoint.y - firstPoint.y
+	return math.degrees(math.atan2(yDiff,xDiff))
+
+class LineUp(FilterWithoutDialog):
+	
+	@objc.python_method
+	def settings(self):
+		self.menuName = Glyphs.localize({
+			'en': 'Line Up Selection',
+			'de': 'Punkte auffädeln',
+			# 'fr': 'Mon filtre',
+			'es': 'Formar línea',
+			'pt': 'Formar linha',
+			'jp': '直線化',
+			# 'ko': '내 필터',
+			'zh': '📐选中点直线对齐',
+			})
+
+	@objc.python_method
+	def filter(self, layer, inEditView, customParameters):
+		if inEditView and layer.selection:
+			points = [item for item in layer.selection if type(item) in (GSNode, GSAnchor)]
+			if len(points) > 2:
+				if layer.selectionBounds.size.width > layer.selectionBounds.size.height:
+					points = sorted( points, key = lambda thisListItem: thisListItem.x )
+				else:
+					points = sorted( points, key = lambda thisListItem: thisListItem.y )
+	
+				firstPoint = points[0]
+				lastPoint = points[-1]
+				angle = calculateAngle( firstPoint, lastPoint )
+				rotation = transform(rotate=-angle).transformStruct()
+				layer.applyTransform(rotation)
+				alignX = firstPoint.x == lastPoint.x
+
+				for point in points[1:-1]:
+					if alignX:
+						point.x = firstPoint.x
+					else:
+						point.y = firstPoint.y
+		
+				rotationBack = transform(rotate=angle).transformStruct()
+				layer.applyTransform(rotationBack)
+
+	@objc.python_method
+	def __file__(self):
+		"""Please leave this method unchanged"""
+		return __file__
--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # LineUp
+
 Glyphs.app filter for lining up nodes and anchors in your point selection
+
+This is a plug-in for the [Glyphs font editor](https://glyphsapp.com/). It lines up nodes, handles and anchors in your point selection between the outermost points selected.
+
+![Line Up Screenshot.](LineUp.png "Line Up, before and after")
+
+### Installation
+
+1. In *Window > Plugin Manager,* look for *Line Up* and click *Install.*
+2. Restart Glyphs
+
+### Usage Instructions
+
+1. Open a glyph in Edit View, and select three or more nodes.
+2. Choose *Filter > Line Up.*
+
+### Requirements
+
+The plugin needs Glyphs 3 or higher, running on OS X 10.11 or later.
+
+### License
+
+Copyright 2022 Rainer Erich Scheichelbauer (@mekkablue).
+Based on sample code by Georg Seifert (@schriftgestalt).
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+See the License file included in this repository for further details.
